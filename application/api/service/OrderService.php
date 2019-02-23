c0887333bdd2c4457e0e865c9b957030b4d4d494
MadyMorning <chen790119521@gmail.com>
2019-02-23 12:16:20 +0800

    订单部分编写（未完成）

--- application/api/service/OrderService.php
+++ application/api/service/OrderService.php
@@ -2,30 +2,32 @@
 namespace app\api\service;
 use app\api\model\ProductModel;
 use exception\RequestFailedException;
+use exception\ResourceException;
+
 /**
  * 订单service
  */
 class OrderService
 {
   protected $order_products;  //客户端发送来的商品信息
-  protected $product;   //数据库中的商品信息
+  protected $products;   //数据库中的商品信息
   protected $uid;   //用户ID
 
 
   public function placeOrder($uid, $order_products)
   {
     $this->order_products = $order_products;
-    $this->uid = $uid;
-    $this->product = $this->getProductByOrder($order_products);
+    $this->uid            = $uid;
+    $this->products       = $this->getProductByOrder($order_products);
 
-    // foreach ($order_products as $value) {
-    //   $this->product = ProductModel::find($value['product_id']);
-    //   if ($this->product->stock - $value['count'] < 0) {
-    //     throw new RequestFailedException('所选商品库存量不足');
-    //   }
-    // }
+    $status = $this->getOrderStatus();
+    // 若库存量检测未通过
+    if (!$status['pass']) {
+      $status['order_id'] = -1;
+      return $status;
+    }
 
-
+    // 创建订单
   }
 
   /**
@@ -43,19 +45,73 @@ class OrderService
 
     // 根据商品ID查询数据
     try {
-      $product = ProductModel::with('Image')->select($order_PID)->hidden('summary')->toArray();
-      return $product;
+      $products = ProductModel::with('Image')->select($order_PID)->hidden('summary')->toArray();
+      return $products;
     } catch (\Exception $e) {
       throw new RequestFailedException($e->getMessage());
     }
   }
 
+  /**
+   * 获取订单状态
+   * @return array 返回订单详细信息
+   */
   private function getOrderStatus()
   {
     $status = [
-      'pass' => true,       //库存量检测是否通过
-      'orderPrice' => 0,    //总价格
-      'pStatusArray' => []  //商品详细信息
+      'pass'         => true, //库存量检测是否通过
+      'orderPrice'   => 0,    //订单总价格
+      'pStatusArray' => []    //商品详细信息
     ];
+
+    foreach ($this->order_products as $value) {
+      $pStatus = $this->getProductStatus($value['product_id'], $value['count'], $this->products);
+
+      if (!$pStatus['haveStock']) {
+        $status['pass'] = false;
+      }
+      $status['orderPrice'] += $pStatus['eachPrice'];
+      array_push($status['pStatusArray'], $pStatus);
+    }
+
+    return $status;
+  }
+
+  /**
+   * 获取商品状态
+   * @param  string $PID     订单中商品ID
+   * @param  string $count   订单中商品数量
+   * @param  array $product  数据库中商品信息
+   * @return array           返回商品状态
+   */
+  private function getProductStatus($oPID, $oCount, $products)
+  {
+    $pStatus = [
+      'id'        => null,   //商品ID
+      'haveStock' => false,  //商品是否有库存
+      'count'     => 0,      //商品购买数量
+      'name'      => '',     //商品名称
+      'eachPrice' => 0       //每一种商品的总价
+    ];
+
+    // 将从客户端发送来的商品ID与数据库中商品ID对应，并保存
+    for ($i=0; $i < count($products); $i++) {
+      if ($PID == $products[$i]['id']) {
+        $pIndex = $i;
+      }
+    }
+    // 若无对应，抛出异常
+    if (!isset($pIndex)) {
+      throw new ResourceException('创建订单失败，订单中含有不存在或已失效的商品！');
+    }
+
+    $product              = $products[$pIndex];
+    $pStatus['id']        = $product['id'];
+    $pStatus['haveStock'] = ($product['stock'] - $oCount >= 0);
+    $pStatus['count']     = $oCount;
+    $pStatus['name']      = $product['name'];
+    $pStatus['eachPrice'] = $product['price'] * $oCount;
+
+    return $pStatus;
   }
 }
